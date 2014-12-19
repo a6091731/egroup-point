a79bc39c3835780beafe7a8b64820cf1be395fc5
egroup.yiru@gmail.com <egroup.yiru@gmail.com@30324ae4-27d8-e40d-a2a4-be5adb4927de>
2014-12-19 06:24:04 +0000

    RWD圖表

---
+++ trunk/Epoint/src/main/java/com/epoint/webapp/controller/ChartController.java
@@ -0,0 +1,38 @@
+package com.epoint.webapp.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ChartController {
+	ApplicationContext context =  new ClassPathXmlApplicationContext("spring-module.xml");
+	
+	@RequestMapping(value = "/showChart", method = RequestMethod.GET)
+	public ModelAndView getChart(){
+		ModelAndView model = new ModelAndView("chart");
+		List<Map<String, String>> listData = new ArrayList<Map<String, String>>();
+		for(int i = 1;i <= 5;i++){
+			Map<String, String> item = new HashMap<String, String>();
+			item.put("value", String.valueOf(i*100));
+			String color = "#F7464A";
+			if(i % 2 == 0){
+				color = "#FFC870";
+			}
+			item.put("color", color);
+//			item.put("highlight", "#FF5A5E");
+			item.put("label", String.valueOf(i));
+			listData.add(item);
+		}
+		model.addObject("dataList", listData);
+		return model;
+	}
+}
